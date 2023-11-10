c8fdc5f431175667175530a20c1491ebd60a8699
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-11-10 16:13:56 +0100

    added all components and imported

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  }
+}
 
 </script>
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,12 @@
+<script>
+import AppCard from './AppCard.vue';
+
+export default {
+    components: { AppCard }
+}
+
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
